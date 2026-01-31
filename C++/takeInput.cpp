8ec88fbe54826b9cf3d6b48c4475d56ddab90a02
Swapnil Motghare <swapnilmotghare44@gmail.com>
2026-01-31 16:14:52 +0530

    Added second file

---
+++ C++/takeInput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int age;
+    string name;
+
+    cout << "enter name ";
+    cin >> name;
+
+    cout << "enter age ";
+    cin >> age;
+
+    cout << "Hello I'm " <<name<<" ,age "<<age;
+}
