e007e448bff8ea76823c9c4a92cec5757ee03588
Swapnil Motghare <swapnilmotghare44@gmail.com>
2026-02-02 18:51:39 +0530

    Added twoPointerReverse.js file

---
+++ Javascript/twoPointerReverse.js
@@ -0,0 +1,19 @@
+
+
+function reverseArr(arr) {
+
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        left++;
+        right--;
+    }
+    return arr;
+}
+
+console.log(reverseArr([4, 5, 6, 7, 8, 4, 2, 88, 7, 993, 3, 1]));
